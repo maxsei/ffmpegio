77363460d327c82d7d85a581b5340320fe67ce61
Max Schulte <maximilliangschulte@pm.me>
2022-07-28 17:24:09 -0500

    unpinned nix pkgs

--- shell.nix
+++ shell.nix
@@ -1,20 +1,12 @@
 #...
-{ } :
+{ pkgs ? import <nixpkgs> {} } :
   let
-    # Pinned nixpkgs
-    pkgs = import (builtins.fetchGit {
-      name = "nixpkg-22.05";                         
-      url = "https://github.com/NixOS/nixpkgs/";             
-      ref = "refs/tags/22.05";           
-      rev = "ce6aa13369b667ac2542593170993504932eb836";                       
-    }) {};                                       
   in
     pkgs.mkShell {
       nativeBuildInputs = with pkgs.buildPackages; [ 
 	git-lfs
 	clang
 	pkg-config
-	gef
 	gcc
         ffmpeg
       ];
