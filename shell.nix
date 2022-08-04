796708f0c20547d585b094b576048fb3198df422
Max Schulte <maximilliangschulte@pm.me>
2022-08-04 17:42:04 -0500

    google vimrc standard

--- shell.nix
+++ shell.nix
@@ -8,6 +8,10 @@
       ref = "refs/tags/22.05";           
       rev = "ce6aa13369b667ac2542593170993504932eb836";                       
     }) {};                                       
+    google-neovim = pkgs.neovim.override {
+	configure.customRC = builtins.readFile(./.vimrc);
+	vimAlias = true;
+    };
   in
     pkgs.mkShell {
       nativeBuildInputs = with pkgs.buildPackages; [ 
@@ -19,5 +23,6 @@
         ffmpeg
 	go
 	delve
+	google-neovim
       ];
     }
