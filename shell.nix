091327119dbf5a8d98829b80dd384b3b8899c255
Max Schulte <maximilliangschulte@pm.me>
2022-08-04 18:14:28 -0500

    formatting

--- shell.nix
+++ shell.nix
@@ -1,28 +1,30 @@
 #...
-{ } :
-  let
-    # Pinned nixpkgs
-    pkgs = import (builtins.fetchGit {
-      name = "nixpkg-22.05";                         
-      url = "https://github.com/NixOS/nixpkgs/";             
-      ref = "refs/tags/22.05";           
-      rev = "ce6aa13369b667ac2542593170993504932eb836";                       
-    }) {};                                       
-    google-neovim = pkgs.neovim.override {
-	configure.customRC = builtins.readFile(./.vimrc);
-	vimAlias = true;
-    };
-  in
-    pkgs.mkShell {
-      nativeBuildInputs = with pkgs.buildPackages; [ 
-	git-lfs
-	clang
-	pkg-config
-	gef
-	gcc
-        ffmpeg
-	go
-	delve
-	google-neovim
-      ];
-    }
+{}:
+let
+  # Pinned nixpkgs
+  pkgs = import
+    (builtins.fetchGit {
+      name = "nixpkg-22.05";
+      url = "https://github.com/NixOS/nixpkgs/";
+      ref = "refs/tags/22.05";
+      rev = "ce6aa13369b667ac2542593170993504932eb836";
+    })
+    { };
+  google-neovim = pkgs.neovim.override {
+    configure.customRC = builtins.readFile (./.vimrc);
+    vimAlias = true;
+  };
+in
+pkgs.mkShell {
+  nativeBuildInputs = with pkgs.buildPackages; [
+    git-lfs
+    clang
+    pkg-config
+    gef
+    gcc
+    ffmpeg
+    go
+    delve
+    google-neovim
+  ];
+}
